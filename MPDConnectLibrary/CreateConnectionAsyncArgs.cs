6c2a491de7abfd45a873701f1fcce20c051af370
Matti Ahinko <matti.ahinko@gmail.com>
2012-11-02 17:49:03 +0200

    mpdconnectia ja dokkariconffi

---
+++ MPDConnectLibrary/CreateConnectionAsyncArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MPDConnectLibrary
+{
+    public class CreateConnectionAsyncArgs : EventArgs
+    {
+        public bool ConnectionEstablished { get; private set; }
+
+        public CreateConnectionAsyncArgs(bool connectionEstablished)
+        {
+            ConnectionEstablished = connectionEstablished;
+        }
+    }
+}
--- MPDConnectLibrary/MPDClient.cs
+++ MPDConnectLibrary/MPDClient.cs
@@ -19,6 +19,7 @@
  */
 using System;
 using System.Net;
+using System.Net.Sockets;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
@@ -32,6 +33,101 @@ namespace MPDConnectLibrary
 {
     public class MPDClient
     {
+        public static string NEXT = "next";
+        public static string PREVIOUS = "previous";
+        public static string SETVOL = "setvol";
+        public static string STOP = "stop";
+        public static string PLAY = "play";
+        public static string PAUSE = "pause";
+        public static string SEEK = "seek";
 
+        public event EventHandler<CreateConnectionAsyncArgs> CreateConnectionCompleted;
+
+        private Socket connection;
+
+        private string address;
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        private int port;
+
+        public int Port
+        {
+            get { return port; }
+            set { port = value; }
+        }
+
+        private string username;
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        private string password;
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+
+        private bool isConnected;
+
+        public bool IsConnected
+        {
+            get { return isConnected; }
+            set { isConnected = value; }
+        }
+
+        public int Connect()
+        {
+            return this.Connect(this.address, this.port);
+        }
+
+        public int Connect(string serverAddress, int port)
+        {
+            this.connection = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            var connectionOperation = new SocketAsyncEventArgs { RemoteEndPoint = new DnsEndPoint(serverAddress, port) };
+            connectionOperation.Completed += OnConnectionToServerCompleted;
+            this.connection.ConnectAsync(connectionOperation);
+            return 0;
+        }
+
+        private void OnConnectionToServerCompleted(object sender, SocketAsyncEventArgs e)
+        {
+            if (e.SocketError != SocketError.Success)
+            {
+                if (CreateConnectionCompleted != null)
+                    CreateConnectionCompleted(this, new CreateConnectionAsyncArgs(false));
+
+                return;
+            }
+
+            if (CreateConnectionCompleted != null)
+                CreateConnectionCompleted(this, new CreateConnectionAsyncArgs(true));
+        }
+
+
+
+        public int Disconnect()
+        {
+
+            return 0;
+        }
+
+        public int SendCommand(string command, string[] attributes)
+        {
+            return 0;
+        }
+
+        
     }
 }
